67306cd4c873dbff6b0d5f86605c101dba5e681c
zevatron <zevatron@zevatron.com.br>
2017-07-05 00:46:10 -0300

    #7 - Projeto Maven criado baseado na prática do Memoriam. Configuração do persistence.xml para conectar no PostgreSQL. Implementação da classe Usuario.java (copiada do memoriam) e DAO's. Teste de conexão com banco com sucesso. Tabela foi criada com sucesso pelo Hibernate. Facets ajustado para Java 8.

---
+++ projeto_tcc/src/main/java/br/edu/ifpb/tcc/entity/Usuario.java
@@ -0,0 +1,78 @@
+package br.edu.ifpb.tcc.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USUARIO")
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="ID")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name="NOME")
+	private String nome;
+	
+	@Column(name="EMAIL")
+	private String email;
+	
+	@Column(name="SENHA")
+	private String senha;
+	
+	@Column(name="ATIVO")
+	private Boolean ativo;
+	
+	public String getPrenome() {
+		return  (this.nome != null) ? this.nome.split(" ")[0].trim() : null;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
